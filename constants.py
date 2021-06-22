8b0575d19ba1896a5010db1f7e11c2eeb25bf65d
CJSanon <36585366+CJSanon@users.noreply.github.com>
2021-06-22 18:03:48 -0400

    Removed keys

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
-CONSUMER_KEY = 'nFFesGST4NJcPpqiRiBmZhm34'
-CONSUMER_SECRET = 'rqYCocmfBj66KnlL7QjljwoGIvJLlPqFgj4yvdjGklRi10TmAH'
+CONSUMER_KEY = 
+CONSUMER_SECRET =
 
 REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
 ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
